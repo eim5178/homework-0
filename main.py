5b41930433d46e0b75304f0be0cf644badea98d3
eim5178 <eim5178@psu.edu>
2020-08-28 11:17:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def main(): 
+  celsius_temperature = float(input("Enter the temperature in celsius: "))
+  temperature = (celsius_temperature  * 9/5) + 32
+  print(celsius_temperature, "° in celsius is equivalent to", temperature, "°" )
+main()
