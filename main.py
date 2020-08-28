38e05532c12931c83723038df725fd089f99db4f
eim5178 <eim5178@psu.edu>
2020-08-28 11:25:58 +0000

    degree sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main(): 
   celsius_temperature = float(input("Enter the temperature in celsius: "))
-  temperature = (celsius_temperature  * 9/5) + 32
-  print(celsius_temperature, "° in celsius is equivalent to", temperature, "°" )
+  temperature = (celsius_temperature  * 9/5) + 32 
+  print(celsius_temperature, "\N{degree sign} in celsius is equivalent to" , temperature, "\N{degree sign}  Fahrenheit." )
 main()
