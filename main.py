c79108e805e5d43aa12196b5f0493f954368effc
eim5178 <eim5178@psu.edu>
2020-08-28 11:32:05 +0000

    deleting the word the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main(): 
-  celsius_temperature = float(input("Enter the temperature in celsius: "))
+  celsius_temperature = float(input("Enter temperature in celsius: "))
   temperature = (celsius_temperature  * 9/5) + 32 
-  print(str(celsius_temperature) + "\N{degree sign} in celsius is equivalent to " + str(temperature) + "\N{degree sign} Fahrenheit." )
+  print(str(celsius_temperature) + "\N{degree sign} in Celsius is equivalent to " + str(temperature) + "\N{degree sign} Fahrenheit." )
 main()
